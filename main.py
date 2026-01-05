52bf7e44fd8392e4cad29ff0d4f92624133b5395
cerdise12 <tok141844@gmail.com>
2026-01-05 18:50:30 +0300

    Register admin handlers in main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import telebot
 from config import BOT_TOKEN
 from handlers import register_handlers
+from admin import register_admin 
 
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
 
 register_handlers(bot)
+register_admin(bot)      
 
 bot.infinity_polling(skip_pending=True)
